94935b4228c05387d20e2c6360fa4fea82d8b36b
JPDTD21L4066\334483 <daiki.nakajima@tohmatsu.co.jp>
2023-07-25 16:31:17 +0900

    no message

--- test/unit/int_citest_d/scripts/util/citest/amountCheck.js
+++ test/unit/int_citest_d/scripts/util/citest/amountCheck.js
@@ -4,7 +4,7 @@ const assert = require('chai').assert;
 
 describe('amountCheck', function() {
     const util = require('../../../../../../cartridges/int_citest_d/cartridge/scripts/util/citest/amoutCheck.js')
-    const num = 123456789;
+    const num = 1234567890;
 
     it('isTenDigit', function() {
         const result = util.isTenDigit(num);
